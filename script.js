8cc5d734d9c6af08888c24b5c83df7f76bec8cd6
M's Project <108556830+M-joey@users.noreply.github.com>
2022-09-19 14:37:37 +1000

    contact form

--- script.js
+++ script.js
@@ -106,3 +106,14 @@ while (index < navList.length){
   })
   index++;
 }
+
+//contact form
+const scriptURL = '<SCRIPT URL>'
+  const form = document.forms['submit-to-google-sheet']
+
+  form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+      .then(response => console.log('Success!', response))
+      .catch(error => console.error('Error!', error.message))
+  })
